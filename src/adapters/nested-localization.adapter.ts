fe113941e1464df6bc8c83d26e84e77ed9fc386e
SocketSomeone <socket.someone@gmail.com>
2024-04-13 20:51:49 +0300

    style: lint

--- src/adapters/nested-localization.adapter.ts
+++ src/adapters/nested-localization.adapter.ts
@@ -1,27 +1,35 @@
 import { BaseLocalizationAdapter } from './base-localization.adapter';
 
 interface TranslationData {
-    [key: string]: string | TranslationData;
+	[key: string]: string | TranslationData;
 }
 
 interface NestedLocalizationAdapterOptions {
-    fallbackLocale?: string;
-    locales?: Record<string, TranslationData>;
+	fallbackLocale?: string;
+	locales?: Record<string, TranslationData>;
 }
 
 export class NestedLocalizationAdapter extends BaseLocalizationAdapter<NestedLocalizationAdapterOptions> {
-    public getTranslation(key: string, locale: string, placeholders?: Record<string, string>): string {
-        const translations = this.getTranslations(locale);
-        const translation = this.findTranslation(translations, key) ?? this.getFallbackTranslation(key);
+	public getTranslation(
+		key: string,
+		locale: string,
+		placeholders?: Record<string, string>
+	): string {
+		const translations = this.getTranslations(locale);
+		const translation =
+			this.findTranslation(translations, key) ?? this.getFallbackTranslation(key);
 
-        return translation.replace(/{{\s*([^}\s]+)\s*}}/g, (_, placeholder) => placeholders?.[placeholder] ?? '')
-    }
+		return translation.replace(
+			/{{\s*([^}\s]+)\s*}}/g,
+			(_, placeholder) => placeholders?.[placeholder] ?? ''
+		);
+	}
 
-    private getTranslations(locale: string): TranslationData {
-        return this.options?.locales?.[locale] ?? {};
-    }
+	private getTranslations(locale: string): TranslationData {
+		return this.options?.locales?.[locale] ?? {};
+	}
 
-    private findTranslation(translations: TranslationData, key: string): string | undefined {
+	private findTranslation(translations: TranslationData, key: string): string | undefined {
 		const keys = key.split('.');
 		let currentTranslation: string | TranslationData | undefined = translations;
 
@@ -36,11 +44,10 @@ export class NestedLocalizationAdapter extends BaseLocalizationAdapter<NestedLoc
 		return typeof currentTranslation === 'string' ? currentTranslation : undefined;
 	}
 
+	private getFallbackTranslation(key: string): string {
+		const fallbackLocale = this.options?.fallbackLocale ?? 'en-US';
+		const translations = this.getTranslations(fallbackLocale);
 
-    private getFallbackTranslation(key: string): string {
-        const fallbackLocale = this.options?.fallbackLocale ?? 'en-US';
-        const translations = this.getTranslations(fallbackLocale);
-
-        return this.findTranslation(translations, key) ?? key;
-    }
+		return this.findTranslation(translations, key) ?? key;
+	}
 }
